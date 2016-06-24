f01a8623233a47df2030e7b3cfa1edb0d1f63213
Casper Bollen <halcwb@gmail.com>
2016-06-24 10:21:13 +0200

    update node and npm on travis

--- npm_update.sh
+++ npm_update.sh
@@ -4,7 +4,14 @@
 node -v
 # Get the current npm versino
 npm -v
-# sudo npm -g install npm@3
-# sudo npm cache clean -f
-# sudo npm install -g n
-# sudo n "$@"
+
+# Update to the latest stable version
+sudo npm -g install npm@3
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
+# Get the current node version
+node -v
+# Get the current npm versino
+npm -v
