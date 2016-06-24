d2e9ddc60848c9795b72107c9a1905495e415e9d
Casper Bollen <halcwb@gmail.com>
2016-06-24 11:40:58 +0200

    Now update node on max and linux

--- npm_update.sh
+++ npm_update.sh
@@ -11,19 +11,19 @@ if [[ "$OSTYPE" == "linux"* ]]; then
     # Linux
     echo OS: Linux
 
-    # curl #s #o $HOME/.nvm/nvm.sh https://raw.githubusercontent.com/creationix/nvm/v0.31.0/nvm.sh
-    # source $HOME/.nvm/nvm.sh
-    # nvm install stable
-    # node ##version
+    # Update to the latest stable version
+    curl -s -o $HOME/.nvm/nvm.sh https://raw.githubusercontent.com/creationix/nvm/v0.31.0/nvm.sh
+    source $HOME/.nvm/nvm.sh
+    nvm install stable
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     echo OS: Mac
 
     # Update to the latest stable version
-    # sudo npm cache clean -f
-    # sudo npm install -g n
-    # sudo n stable
+    sudo npm cache clean -f
+    sudo npm install -g n
+    sudo n stable
 fi
 
 # Get the updated node version
