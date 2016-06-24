400bd4a0b51ca5d8a1e4453bb529d9717087e48f
Casper Bollen <halcwb@gmail.com>
2016-06-24 11:04:52 +0200

    Try update node in yaml

--- npm_update.sh
+++ npm_update.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Add path to node 
+# export PATH=""
+
 # Get the path variable
 echo $PATH
+echo
 
 # Get the current node version
 node -v
@@ -9,11 +13,7 @@ node -v
 npm -v
 
 # Update to the latest stable version
-sudo npm cache clean -f
-sudo npm install -g n
-sudo n stable
+# sudo npm cache clean -f
+# sudo npm install -g n
+# sudo n stable
 
-# Get the current node version
-node -v
-# Get the current npm versino
-npm -v
