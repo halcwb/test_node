938df0654d3bf2063eb41d334b3d1774175c1e30
Casper Bollen <halcwb@gmail.com>
2016-06-24 09:31:41 +0200

    First commit

---
+++ update_npm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Get the current node version
+node -v
+# Get the current npm versino
+npm -v
+# sudo npm -g install npm@3
+# sudo npm cache clean -f
+# sudo npm install -g n
+# sudo n "$@"
