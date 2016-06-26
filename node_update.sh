87953eafd0447b975c2b35617181af94edb65ea9
Casper Bollen <halcwb@gmail.com>
2016-06-26 12:37:53 +0200

    update node to specified version or default stable

--- node_update.sh
+++ node_update.sh
@@ -7,23 +7,25 @@ node -v
 echo npm version:
 npm -v
 
+VERSION=${1:-"stable"}  
+
 if [[ "$OSTYPE" == "linux"* ]]; then
     # Linux
     echo OS: Linux
 
-    # Update to the latest stable version
+    # Update to the supplied version or latest stable version
     curl -s -o $HOME/.nvm/nvm.sh https://raw.githubusercontent.com/creationix/nvm/v0.31.0/nvm.sh
     source $HOME/.nvm/nvm.sh
-    nvm install stable
+    nvm install VERSION
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
     echo OS: Mac
 
-    # Update to the latest stable version
+    # Update to the supplied version or latest stable version
     sudo npm cache clean -f
     sudo npm install -g n
-    sudo n stable
+    sudo n VERSION
 fi
 
 # Get the updated node version
